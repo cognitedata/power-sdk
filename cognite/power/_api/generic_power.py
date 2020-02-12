02dfa7deefe065e30c3649565a35e9270474822d
Sander Land <sander.land@cognite.com>
2020-02-12 16:28:53 +0100

    name fn

--- cognite/power/_api/generic_power.py
+++ cognite/power/_api/generic_power.py
@@ -14,6 +14,6 @@ class GenericPowerAPI(AssetsAPI):
         filters["metadata"] = {**filters.get("metadata", {}), **self.metadata_filter}
         return PowerAssetList._load_assets(super().list(limit=limit, **filters), cognite_client=self._cognite_client)
 
-    def search_exact(self, name):
+    def retrieve_name(self, name):
         result = assert_single_result(super().list(name=name, metadata=self.metadata_filter))
         return PowerAsset._load_from_asset(result, self._cognite_client)
