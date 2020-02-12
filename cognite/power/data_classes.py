80b273014d0dbcfa16b00383019d8505d1b50e0b
Sander Land <sander.land@cognite.com>
2020-02-12 13:22:27 +0100

    shortcuts

--- cognite/power/data_classes.py
+++ cognite/power/data_classes.py
@@ -6,8 +6,11 @@ from cognite.power.exceptions import WrongPowerTypeError
 
 
 class PowerAsset(Asset):
+    """Extended asset class for Power related resources"""
+
     @property
     def type(self):
+        """Shortcut for it's metadata-defined type (e.g. 'TransformerEnd')"""
         return (self.metadata or {}).get("type")
 
     @property
@@ -29,6 +32,7 @@ class PowerAsset(Asset):
         return dump
 
     def relationship_sources(self, power_type, resource_type="Asset", relationship_type="belongsTo"):
+        """Shortcut for finding all assets that are a source, with the current asset as a target"""
         rels = self._cognite_client.relationships.list(
             source_resource=resource_type,
             target_resource="Asset",
@@ -39,6 +43,7 @@ class PowerAsset(Asset):
         return self._filter_and_convert([r.source["resourceId"] for r in rels], power_type)
 
     def relationship_targets(self, power_type, resource_type="Asset", relationship_type="belongsTo"):
+        """Shortcut for finding all assets that are a target, with the current asset as a source"""
         rels = self._cognite_client.relationships.list(
             target_resource=resource_type,
             source_resource="Asset",
@@ -60,16 +65,47 @@ class PowerAsset(Asset):
             assets = [a for a in assets if (a.metadata or {}).get("type") == power_type]
         return PowerAssetList._load_assets(assets, cognite_client=self._cognite_client)
 
+    def terminal(self):
+        """Shortcut for finding the associated Terminal for a Substation, PowerTransformerEnd, or SynchronousMachine"""
+        if self.type not in ["Substation", "PowerTransformerEnd", "SynchronousMachine"]:
+            raise WrongPowerTypeError("A PowerAsset of type {} does not have a Terminal".format(self.type))
+        return self.relationship_sources("Terminal", relationship_type="connectsTo")[0]
+
+    def analogs(self):
+        """Shortcut for finding the associated Analogs for a Terminal (or any PowerAsset which has a Terminal)"""
+        if self.type != "Terminal":
+            return self.terminal().analogs()
+        return self.relationship_sources("Analog")
+
     def transformer_end(self):
+        """Shortcut for finding the associated PowerTransformerEnd for a PowerTransformer"""
         if self.type not in ["PowerTransformer"]:
-            return self.relationship_targets("PowerTransformer")[0]
+            raise WrongPowerTypeError("A PowerAsset of type {} does not have a PowerTransformerEnd".format(self.type))
+        return self.relationship_targets("PowerTransformerEnd")[0]
 
     def substation(self):
+        """Shortcut for finding the associated transformer for a PowerTransformer, PowerTransformerEnd, .."""
         if self.type == "PowerTransformerEnd":
             return self.relationship_sources("PowerTransformer")[0].substation()
-        if self.type not in ["PowerTransformer"]:
-            raise WrongPowerTypeError("A PowerAsset of type {} does not have a substation".format(self.type))
+        if self.type == "SynchronousMachine":
+            return self.generator().substation()
+        # if self.type != ["PowerTransformer"] and not slef.:
+        #    raise WrongPowerTypeError("A PowerAsset of type {} does not have a substation".format(self.type))
         return self.relationship_sources("Substation")[0]
+
+    def line_segments(self):
+        """Shortcut for finding the connected ACLineSegments for a substation"""
+        if self.type != "Substation":
+            raise WrongPowerTypeError("Can only find the lines for a substation, not for a  {}.".format(self.type))
+        return self.relationship_sources("ACLineSegment")
+
+    def connected_terminals(self):
+        """Shortcut for finding the connected Terminals for an ACLineSegment"""
+        if self.type != ["ACLineSegment"]:
+            raise WrongPowerTypeError(
+                "Can only find connected terminals to an ACLineSegment, not a {}.".format(self.type)
+            )
+        return self.relationship_targets("Terminal", relationship_type="connectsTo")
 
 
 class PowerAssetList(AssetList):
