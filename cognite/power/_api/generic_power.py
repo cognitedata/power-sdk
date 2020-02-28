ac8c050732f28b8135857fde8f72b21be22b0620
Sander Land <sander.land@cognite.com>
2020-02-28 13:24:55 +0100

    filter by bidding area

--- cognite/power/_api/generic_power.py
+++ cognite/power/_api/generic_power.py
@@ -10,11 +10,30 @@ class GenericPowerAPI(AssetsAPI):
         super().__init__(config, api_version, cognite_client)
         self.metadata_filter = metadata_filter
 
-    def list(self, grid_type: str = None, base_voltage: Iterable = None, limit: int = None, **filters):
+    def _all_bidding_areas(self):
+        return self._cognite_client.assets.list(metadata={"type": "BiddingArea"})
+
+    def list(
+        self,
+        grid_type: str = None,
+        base_voltage: Iterable = None,
+        bidding_areas: list = None,
+        limit: int = None,
+        **filters,
+    ):
+        if bidding_areas:
+            subtree_assets = [a for a in self._all_bidding_areas() if a.name in bidding_areas]
+            found_names = [a.name for a in subtree_assets]
+            not_found = set(bidding_areas) - set(found_names)
+            if not_found:
+                raise ValueError(f"Bidding area(s) {not_found} not found")
+            subtree_ids = [a.id for a in subtree_assets]
+        else:
+            subtree_ids = None
         filters["metadata"] = {**filters.get("metadata", {}), **self.metadata_filter}
         if grid_type:
             filters["metadata"]["Equipment.gridType"] = grid_type
-        assets = super().list(limit=limit, **filters)
+        assets = super().list(asset_subtree_ids=subtree_ids, limit=limit, **filters)
         if base_voltage:
             assets = [a for a in assets if int((a.metadata or {}).get("BaseVoltage")) in base_voltage]
         return PowerAssetList._load_assets(assets, cognite_client=self._cognite_client)
