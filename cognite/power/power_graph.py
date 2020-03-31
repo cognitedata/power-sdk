40db1fbdf7c27baa74b1842091eb6f0abeb6a289
Sander Land <sander.land@cognite.com>
2020-03-31 14:01:07 +0200

    stuff

--- cognite/power/power_graph.py
+++ cognite/power/power_graph.py
@@ -5,31 +5,41 @@ from matplotlib import pyplot as plt
 
 
 class PowerGraph:
-    def __init__(self):
-        self.load()
+    def __init__(self, cognite_client, subgraph_nodes=None, full_graph=None):
+        self._cognite_client = cognite_client
+        if subgraph_nodes:
+            self.G = nx.Graph()
+            self.G.add_nodes_from(nodes)
+            self.G.add_edges_from(
+                (n, nbr, d) for n, nbrs in self.G.adj.items() if n in nodes for nbr, d in nbrs.items() if nbr in nodes
+            )
+            self.full_graph = full_graph
+        else:
+            self.load()
+            self.full_graph = self
 
     def load(self):
-        ssl = client.substations.list()
-        acl = client.ac_line_segments.list()
+        ssl = self._cognite_client.substations.list()
+        acl = self._cognite_client.ac_line_segments.list()
         ss_extid = {s.external_id: s for s in ssl if s.external_id}
         acl_extid = {s.external_id: s for s in acl if s.external_id}
 
         terminal_map = defaultdict(list)
-        for rel in client.relationships.list(
+        for rel in self._cognite_client.relationships.list(
             targets=[{"resourceId": s.external_id} for s in acl], relationship_type="connectsTo", limit=None
         ):
             terminal = rel.source["resourceId"]
             acls = rel.target["resourceId"]
             terminal_map[terminal].append(acls)
 
-        for check_terminal in client.assets.retrieve_multiple(external_ids=list(terminal_map.keys())):
+        for check_terminal in self._cognite_client.assets.retrieve_multiple(external_ids=list(terminal_map.keys())):
             if check_terminal.metadata.get("type", None) != "Terminal":
                 del terminal_map[check_terminal.external_id]
 
         ss_to_acls_map = defaultdict(list)
         acls_to_ss_map = defaultdict(list)
 
-        for rel in client.relationships.list(
+        for rel in self._cognite_client.relationships.list(
             targets=[{"resourceId": s.external_id} for s in ssl], relationship_type="belongsTo", limit=None
         ):
             ss = rel.target["resourceId"]
@@ -73,6 +83,9 @@ class PowerGraph:
                         node_loc[s] = [20, 55 + orphan_count]  # TODO don't hardcode this
                         orphan_count += 1
         return node_loc
+
+    def select_region(self, nodes):
+        return PowerGraph(nodes, self)
 
     def draw(self):
         nx.draw(self.G, labels={n: n.name for n in self.G.nodes()}, font_size=30, pos=self._node_locations())
