bdc7d68b0cd2c1134ebe55b79e62fa782f7e42b8
andreavs <andreas.solbra@cognite.com>
2020-04-22 13:35:39 +0200

    remove accidental addition

--- cognite/power/data_classes.py
+++ cognite/power/data_classes.py
@@ -322,7 +322,6 @@ class PowerAssetList(AssetList):
     @property
     def type(self) -> Optional[str]:
         """Type of the list of assets, will raise a MixedPowerAssetListException if the list contains several asset types."""
-        self.to_pandas
         if not self.data:
             return None
 
@@ -361,7 +360,6 @@ class PowerAssetList(AssetList):
     def to_power_area(
         self, interior_substation: Union[str, Substation] = None, grid_type: str = None, base_voltage: Iterable = None,
     ):
-        self.to_pandas()
         try:
             if self.type == "Substation":
                 return self._cognite_client.power_area(self)
